3d94c07614f573551d2c31e4b40d99b070046f54
Krystian <krchr@wp.pl>
2024-01-31 21:37:39 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,86 @@
+import express from 'express'
+import { createServer } from 'node:http'
+import { fileURLToPath } from 'node:url'
+import { dirname, join } from 'node:path'
+import { Server } from 'socket.io'
+import sqlite3 from 'sqlite3'
+import { open } from 'sqlite'
+
+const MAX_MESSAGES = 10 // Maximum number of messages to keep
+
+const db = await open({
+	filename: 'chat.db',
+	driver: sqlite3.Database,
+})
+
+await db.exec(`
+  CREATE TABLE IF NOT EXISTS messages (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      client_offset TEXT UNIQUE,
+      content TEXT,
+	  sender TEXT,
+	  time TEXT
+  );
+`)
+
+const app = express()
+const server = createServer(app)
+const io = new Server(server, {
+	connectionStateRecovery: {},
+})
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+app.use('/dist', express.static(join(__dirname, 'dist')));
+app.use('/src', express.static(join(__dirname, 'src')));
+
+app.get('/', (req, res) => {
+	res.sendFile(join(__dirname, 'index.html'))
+})
+
+io.on('connection', async socket => {
+	socket.on('chat message', async (msg, clientOffset, sender, currentDate, callback) => {
+		let result
+		try {
+			result = await db.run('INSERT INTO messages (content, client_offset, sender, time) VALUES (?, ?, ?, ?)', msg, clientOffset, sender, currentDate)
+		} catch (e) {
+			if (e.errno === 19 /* SQLITE_CONSTRAINT */) {
+				callback()
+			} else {
+				// nothing to do, just let the client retry
+			}
+			return
+		}
+		io.emit('chat message', msg, sender, currentDate, result.lastID,)
+
+		const messageCount = await db.get('SELECT COUNT(*) as count FROM messages')
+		if (messageCount.count > MAX_MESSAGES) {
+			// Delete the oldest message
+			await db.run('DELETE FROM messages WHERE id = (SELECT MIN(id) FROM messages)')
+		}
+
+		callback()
+	})
+
+	if (!socket.recovered) {
+		try {
+			await db.each(
+				'SELECT id, content, sender, time FROM messages WHERE id > ?',
+				[socket.handshake.auth.serverOffset || 0],
+				(_err, row) => {
+					socket.emit('chat message', row.content,  row.sender, row.time, row.id)
+				}
+			)
+		} catch (e) {
+			// something went wrong
+		}
+	}
+})
+
+const callback = () => {
+	console.log('Tak o callback')
+}
+
+server.listen(3000, () => {
+	console.log('server running at http://localhost:3000')
+})
